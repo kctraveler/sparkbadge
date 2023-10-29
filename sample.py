9ac34b8c538c645b20fadf16718a4122a052829a
Shane Panchot <shane.panchot@gmail.com>
2023-10-29 14:46:39 -0500

    added dependency injection

--- sample.py
+++ sample.py
@@ -1,30 +1,42 @@
 # This is a sample script of how the package might be used.
+from typing import Any
 import requests
+from itertools import islice
+
+
+class Observer:
+    """Simple base observer for printing output."""
+
+    def update(self, data: Any):
+        print(data)
 
 
 def get_data(url: str):
     """Generator for build coverage data"""
     page = 1
-    data = requests.get(url, params={'page': page}).json()
-    while page <= data['pages']:
-        for build in data['builds']:
+    data = requests.get(url, params={"page": page}).json()
+    while page <= data["pages"]:
+        for build in data["builds"]:
             yield build
         page += 1
-        data = requests.get(url, params={'page': page}).json()
+        data = requests.get(url, params={"page": page}).json()
 
 
-def main():
-    url = "https://coveralls.io/github/kctraveler/github-actions.json"
-    main_branches = filter(lambda build: build["branch"] in ["main", "master"],
-                           get_data(url))
-    parsed_data = map(lambda raw_data: {
+def parse(raw_data: dict) -> dict:
+    return {
         "created_at": raw_data["created_at"],
         "coverage_change": raw_data["coverage_change"],
-        "covered_percent": raw_data["covered_percent"]},
-        main_branches)
+        "covered_percent": raw_data["covered_percent"],
+    }
 
-    print(list(parsed_data))
+
+def run(input: iter, out: Observer, count: int = 10):
+    main_branches = filter(lambda build: build["branch"] in ["main", "master"], input)
+    parsed_data = map(parse, main_branches)
+    recents = islice(parsed_data, count)
+    out.update(list(recents))
 
 
 if __name__ == "__main__":
-    main()
+    url = "https://coveralls.io/github/kctraveler/github-actions.json"
+    run(get_data(url), Observer())
